abc21908e8c603604453c73e373a6d870b10fd3e
shinyay <shinya.com@gmail.com>
2019-09-01 21:49:36 +0900

    Add StreamListener and SendTo annotation function

--- src/main/kotlin/io/pivotal/shinyay/stream/ScStreamGsApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/stream/ScStreamGsApplication.kt
@@ -1,10 +1,21 @@
 package io.pivotal.shinyay.stream
 
+import io.pivotal.shinyay.stream.model.LogMessage
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.cloud.stream.annotation.StreamListener
+import org.springframework.cloud.stream.messaging.Processor
+import org.springframework.messaging.handler.annotation.SendTo
 
 @SpringBootApplication
-class ScStreamGsApplication
+@EnableBinding(Processor::class)
+class ScStreamGsApplication {
+
+	@StreamListener(Processor.INPUT)
+	@SendTo(Processor.OUTPUT)
+	fun logMessage(log: LogMessage) = LogMessage("[1]: ${log.message}")
+}
 
 fun main(args: Array<String>) {
 	runApplication<ScStreamGsApplication>(*args)
