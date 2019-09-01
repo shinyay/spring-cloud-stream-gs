1104064d15c70d34dfe665f2c1ce504722da2dc2
shinyay <shinya.com@gmail.com>
2019-09-01 22:26:06 +0900

    Add simple test for send message

--- src/test/kotlin/io/pivotal/shinyay/stream/ScStreamGsApplicationTests.kt
+++ src/test/kotlin/io/pivotal/shinyay/stream/ScStreamGsApplicationTests.kt
@@ -1,16 +1,32 @@
 package io.pivotal.shinyay.stream
 
+import io.pivotal.shinyay.stream.model.LogMessage
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.stream.messaging.Processor
+import org.springframework.cloud.stream.test.binder.MessageCollector
+import org.springframework.messaging.support.MessageBuilder
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit4.SpringRunner
+import org.junit.Assert.assertEquals
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class ScStreamGsApplicationTests {
+@DirtiesContext
+class ScStreamGsApplicationTests(val pipe: Processor,
+								 val messageCollector: MessageCollector) {
 
 	@Test
-	fun contextLoads() {
+	fun whenSendMessage_thenResponseShouldUpdateText() {
+		val message = MessageBuilder.withPayload(LogMessage("This is my message")).build()
+		pipe.input().send(message)
+
+		val payload = messageCollector.forChannel(pipe.output())
+				.poll()
+				.payload
+
+		assertEquals("[1]: This is my message", payload.toString())
 	}
 
 }
